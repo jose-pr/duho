00104c571dbe2e06a3186873ae0d8a098852b8ba
Jose A <jose-pr@coqui.dev>
2026-07-18 08:39:23 -0400

    chore(release): 0.3.2

--- src/duho/__init__.py
+++ src/duho/__init__.py
@@ -46,7 +46,7 @@ from .qualname import PythonName, QualName
 from .runtime import app, run_command
 from .text import camelcase, expand, gettext, pysafe, snakecase
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 def parser(cls, *args, **kwargs):
